961267a176b7ad8798a48e826b74db4cb6de8fcd
Bernardo-PC <bernardo.vs245@gmail.com>
2025-10-02 11:45:18 -0300

    feat: Create models for entities

--- movies/moviesSite/models.py
+++ movies/moviesSite/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.IntegerField()
+    runtime = models.CharField(max_length=50)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    plot = models.TextField(blank=True)
+    poster = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.title
+
+class MovieList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    movies = models.ManyToManyField(Movie, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.owner.username}"
