f35fafa716bae13fc4e375e7b3c1a83e137c41fb
Bernardo-PC <bernardo.vs245@gmail.com>
2025-10-03 11:43:18 -0300

    feat: Create movie details screen with favorite logic

--- movies/moviesApp/urls.py
+++ movies/moviesApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.movies, name='movies'),
     path("search/", views.movie_search, name="movie_search"),
+    path("movie/<int:movie_id>/", views.movie_detail, name="movie_detail")
 ]
--- movies/moviesApp/views.py
+++ movies/moviesApp/views.py
@@ -16,3 +16,8 @@ def movie_search(request):
     if query:
         results = search_movies(query)
     return render(request, "movies/movie_search.html", {"results": results, "query": query})
+
+def movie_detail(request, movie_id){
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, "movie_detail.html", {"movie": movie})
+}
