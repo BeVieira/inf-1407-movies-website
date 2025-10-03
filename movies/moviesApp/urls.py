0088cfd4aa9ff207968cd4ec01f19661a698f6dd
Bernardo-PC <bernardo.vs245@gmail.com>
2025-10-03 15:23:30 -0300

    feat: Create user profile logic

--- movies/moviesApp/urls.py
+++ movies/moviesApp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.movies, name='movies'),
     path("search/", views.movie_search, name="movie_search"),
-    path("movie/<int:movie_id>/", views.movie_details, name="movie_details")
+    path("movie/<int:movie_id>/", views.movie_details, name="movie_details"),
+
+    path('user/lists/', views.user_lists, name='user_lists'),
+    path('user/profile/', views.user_profile, name='user_profile'),
 ]
