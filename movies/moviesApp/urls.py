627f40ccefed9d41592d3d153f41e0bc79ba96d2
João Victor <jvictor.franci@gmail.com>
2025-10-02 23:03:47 -0300

    feat: implements initial page and movie_search

--- movies/moviesApp/urls.py
+++ movies/moviesApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.movie_list, name='movie_list'),
+    path('', views.movies, name='movies'),
     path("search/", views.movie_search, name="movie_search"),
 ]
